abdfb39259710724944d55c986113c70c8cf602f
KirillKostylev <kirillkostylev2000@gmail.com>
2018-12-11 13:06:17 +0300

    update

--- src/by/bntu/fitr/povt/threadkeepers/javalabs/lab7/tools/UserInput.java
+++ src/by/bntu/fitr/povt/threadkeepers/javalabs/lab7/tools/UserInput.java
@@ -5,35 +5,34 @@ import by.bntu.fitr.povt.threadkeepers.javalabs.lab7.view.View;
 import java.util.Scanner;
 
 public class UserInput {
+    public static String ERROR_MESSAGE = "Input error, all written below is false, the program doesn't work\n";
     private static Scanner scanner = new Scanner(System.in);
 
     public static int inputIntegerPositiveData(String string) {
-        int number;
+        int number = 0;
         View.print(string);
         try {
             number = scanner.nextInt();
             if (number < 0) throw new Exception();
         } catch (Exception e) {
-            View.print("Input error, all written below is false, the program does not work\n");
-            number = 0;
+            View.print(ERROR_MESSAGE);
         }
         return number;
     }
 
     public static double inputDoubleData(String string) {
-        double number;
+        double number = 0;
         View.print(string);
         try {
             number = scanner.nextDouble();
         } catch (Exception e) {
-            View.print("Input error, all written below is false, the program does not work\n");
-            number = 0;
+            View.print(ERROR_MESSAGE);
         }
         return number;
     }
 
-    public static char inputChar (String string) {
-        char letter;
+    public static char inputChar(String string) {
+        char letter = 0;
         String input;
         View.print(string);
         try {
@@ -42,8 +41,7 @@ public class UserInput {
             letter = input.charAt(0);
             if (letter < 65 || letter > 90 && letter < 97 || letter > 122) throw new Exception();
         } catch (Exception e) {
-            View.print("Input error, all written below is false, the program does not work\n");
-            letter = 0;
+            View.print(ERROR_MESSAGE);
         }
         return letter;
     }
